c184eb2e4c2db18645c022a3766cd75320c77d40
ivasilyev1 <vasilyev.ilya3@gmail.com>
2021-12-29 14:44:22 -0500

    - Add fileConfig setup prior to app initialization to provide request logging - Remove unused import

--- usmc-pft-backend/usmc_pft/__init__.py
+++ usmc-pft-backend/usmc_pft/__init__.py
@@ -1,8 +1,13 @@
+from logging.config import fileConfig
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS, logging
+from flask_cors import CORS
+
+fileConfig('./log.conf')
 
 app = Flask(__name__)
+
 CORS(app, resources={r"*":{"origins": "http://localhost:*"}})
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///usmc-pft.db'
 db = SQLAlchemy(app)
